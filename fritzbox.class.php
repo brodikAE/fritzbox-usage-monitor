306812275a4312e52a7bbcf7dc93859d709457f4
brodikAE <112592616+brodikAE@users.noreply.github.com>
2025-04-15 21:54:42 +0200

    Update fritzbox.class.php

--- fritzbox.class.php
+++ fritzbox.class.php
@@ -112,6 +112,8 @@ class FritzBox{
 
 	function initDatabase() {
 		$db = new SQLite3($this->db_file);
+		$db->busyTimeout(5000);
+		$db->exec('PRAGMA journal_mode = WAL;');
 		$db->exec("CREATE TABLE IF NOT EXISTS all_stats (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			mac TEXT NOT NULL,
